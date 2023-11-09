75d4e04f5a628ecd0cef4b5af212c40184590676
Bobby Upreti <bobbyupreti1234@gmail.com>
2023-11-09 11:48:01 +0530

    dasdas

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -8,10 +8,13 @@ const sendToken=(user, statusCode, res)=>{
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000,
         ),
-        // secure: process.env.NODE_ENV==="production",
-        // sameSite: process.env.NODE_ENV==="production"?'Lax':'None',
+        secure: process.env.NODE_ENV==="production",
+        sameSite: process.env.NODE_ENV==="development"?'Lax':'None',
         httpOnly:true,
     };
+
+    // res.cookie("token_ios", token, {}})
+
     res.status(statusCode).cookie("token", token, options).json({
         success:true,
         user,
