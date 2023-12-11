486c8521a4990b05ee4ad5bfad061d7589ba24c7
Bobby Upreti <bobbyupreti1234@gmail.com>
2023-12-12 01:57:41 +0530

    cyclic uploaded

--- server.js
+++ server.js
@@ -56,6 +56,47 @@
 
 //   module.exports=instance;
 
+// const app = require("./app");
+// const cloudinary = require("cloudinary");
+// const connectDatabase = require("./config/database");
+
+// // Handle uncaught exceptions
+// process.on("uncaughtException", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log("Shutting down the server due to uncaught exception");
+//   process.exit(1);
+// });
+
+// // Configuration
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "./config/config.env" });
+// }
+
+// // Connecting to the database
+// connectDatabase();
+
+// // Cloudinary configuration
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(`Server is listening on https://localhost:${process.env.PORT}`);
+// });
+
+// // Unhandled promise rejections
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log("Shutting down the server due to unhandled promise rejection");
+
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+
 const app = require("./app");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
@@ -73,26 +114,28 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // Connecting to the database
-connectDatabase();
+connectDatabase(() => {
+  // The callback is executed once the database connection is established
+  // Cloudinary configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
-// Cloudinary configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+  // Start the server
+  const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on https://localhost:${process.env.PORT}`);
+  });
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on https://localhost:${process.env.PORT}`);
-});
+  // Unhandled promise rejections
+  process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to unhandled promise rejection");
 
-// Unhandled promise rejections
-process.on("unhandledRejection", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log("Shutting down the server due to unhandled promise rejection");
-
-  server.close(() => {
-    process.exit(1);
+    server.close(() => {
+      process.exit(1);
+    });
   });
 });
 
