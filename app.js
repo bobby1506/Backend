c7b978607028d29fc48f4b09a6506125b47cb2c0
Krupal Patel <119075131+KnixSpace@users.noreply.github.com>
2024-07-10 22:42:48 +0530

    Update app.js
    new origin added in cors configurations

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "https://greenglobalaggrovationfrontend.onrender.com",
+      "https://greenglobalaggrovation-8129.onrender.com",
       "https://greenglobalaggrovation.com",
     ],
   })
